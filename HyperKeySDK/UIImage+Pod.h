e48a9d51d79cf04205d67538c1c6850714ffc933
Stanislav Fedorov <>
2019-01-09 00:47:49 +0300

    Initial commit

---
+++ HyperKeySDK/UIImage+Pod.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+Pod.h
+//  HyperKeySDK
+//
+//  Created by Stanislav Fedorov on 08/01/2019.
+//  Copyright © 2019 Stanislav Fedorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage_Pod : UIImage
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HyperKeySDK/UIImage+Pod.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Pod.m
+//  HyperKeySDK
+//
+//  Created by Stanislav Fedorov on 08/01/2019.
+//  Copyright © 2019 Stanislav Fedorov. All rights reserved.
+//
+
+#import "UIImage+Pod.h"
+
+@implementation UIImage_Pod
+
+@end
